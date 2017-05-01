4790b5685a0e72f08b8e5b3bdd58f3259277dbaa
LENOVO-Y400\Kevin <kevin999990@gmail.com>
2017-05-01 15:41:58 +0800

    add List.h

---
+++ List.h
@@ -0,0 +1,15 @@
+#pragma once
+using namespace std;
+
+template <class T>
+class List {
+private:
+	List();
+	Node<T>* root;
+	int count;
+public:
+	//Create()
+	//Retrive()
+	//Update()
+	//Delete()
+};
